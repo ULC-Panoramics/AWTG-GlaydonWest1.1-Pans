740e02d1c484dc36babc31ce16057ea7eaca1ab5
ULC-Panoramics <robert.walter@unitedliving.co.uk>
2024-11-10 21:00:37 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ var viewer = pannellum.viewer('panorama', {
     "scenes": {
         "image-1": {
             "type": "equirectangular",
-            "panorama": "15m.JPG", 
+            "panorama": "20m.JPG", 
             "hfov":200,
             "yaw":0,
             "compass":true,
@@ -18,7 +18,7 @@ var viewer = pannellum.viewer('panorama', {
   
         "image-2": {
         "type": "equirectangular",
-        "panorama": "20m.JPG",
+        "panorama": "25m.JPG",
         "hfov":200,
         "haov": 360,
         "vaov":150,
@@ -31,7 +31,7 @@ var viewer = pannellum.viewer('panorama', {
 
        "image-3": {
          "type": "equirectangular",
-        "panorama": "360.JPG",
+        "panorama": "900m East 15m.JPG",
         "hfov":200,
         "haov": 360,
         "vaov":150,
@@ -44,7 +44,7 @@ var viewer = pannellum.viewer('panorama', {
 
       "image-4":{
         "type": "equirectangular",
-         "panorama": "NA",
+         "panorama": "900m East 20m.JPG",
          "hfov":200,
          "haov": 360,
          "vaov":150,
